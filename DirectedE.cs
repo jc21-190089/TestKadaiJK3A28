8c08835130e244400398a92a0345c3329eb151f1
成喜 外塚 <190089@jc-21.jp>
2022-02-04 11:58:03 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectedE.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class DirectedE
+    {
+        public static int[] SEARCH(int[] f,int[] n,int s)
+        {
+            int i,t,w, top, last, x, temp;
+            int N = f.Count();//点の個数
+            int M = n.Count();//辺の個数
+            int[] start = new int[] {0,1,2,3,4,2,5,4,6 };
+            int[] end = new int[] {0,2,3,4,1,5,4,6,2 };
+
+            //int[] edgefirst = new int[]{ 0, 1, 2, 3, 4, 6, 8 };
+            //int[] edgenext = new int[] { 0, 0, 5, 0, 7, 0, 0, 0, 0 };//動作確認済み、
+            //↓↓
+           /* int[] fcopy = new int[f.Length];
+            Array.Copy(f, fcopy, f.Length);
+
+            int[] ncopy = new int[n.Length];
+            Array.Copy(n, ncopy, n.Length);*/
+
+            int[] edgefirst = new int[N + 1];
+            int[] edgenext = new int[M + 1];
+
+            edgefirst[0] = 0;
+            edgenext[0] = 0;
+
+            for (t = 1; t <=N; t++)
+            {
+                edgefirst[t] = f[t-1];//edgefirst[t] = fcopy[t-1];
+            }
+            for (w = 1; w <= M; w++)
+            {
+                edgenext[w] = n[w-1];//edgenext[w] = ncopy[w-1];
+            }
+            //↑↑
+            
+
+            int[] current = new int[N+1];//配列currentの作成
+            int[] searched = new int[M+1];//配列serchedの作成
+            int[] path = new int[M+1];//配列pathの作成
+
+            for(i=0; i <= N; i++)
+            {
+                current[i] = edgefirst[i];
+            }
+            top = 1;
+            last = M ;
+            x = 1;
+            while(last>=1)
+            {
+                if (current[x] != 0)
+                {
+                    temp = current[x];
+                    searched[top] = temp;
+                    current[x] = edgenext[temp];
+                    x = end[temp];
+                    top = top + 1;
+                }
+                else
+                {
+                    top = top - 1;
+                    temp = searched[top];
+                    path[last] = temp;
+                    x = start[temp];
+                    last = last - 1;
+                }
+            }
+            int[] newpath = new int[M];
+
+     
+            for (int q = 0; q < M; q++)
+            {
+                newpath[q] = path[q + 1];
+            }
+            return newpath;
+
+
+        }
+    }
+}
+
---
+++ JetCoaster.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class JetCoaster
+    {
+        public static int ride(int k,int[] g,int r)
+        {
+            //配列の個数[g.Length-1];
+            //for (int i = 0; i <4; i++)
+            //入れ替えループ
+            /*itizihozon = g[0];
+                g[0] = g[1];
+                g[1] = g[2];
+                g[2] = g[3];
+                g[3] = itizihozon;*/
+
+            //配列のコピー
+            int[] qqq = new int[g.Length];
+            Array.Copy(g, qqq,g.Length);
+
+            int goukei = 0;      
+            int itizihozon = 0;
+            int teiin = k;//teiin=6
+            //rの回数分ループ
+            for (int w = 0; w < r; w++)
+            {
+                k = teiin;
+                int count = 0;
+                //rの1回のループ
+                for (int i = 0; i < g.Length; i++)
+                {
+                    if (k >= g[i])
+                    {
+                        k = k - g[i];
+                        goukei = goukei + g[i];
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                //rで使用したcountをもとに配列を回転させる
+                for (int j = 0; j < count; j++)
+                {
+                    itizihozon = g[0];
+                    for (int q = 0; q < g.Length; q++)
+                    {
+                        if (q == g.Length - 1)//if (q == 3)4回目,
+                        {
+                            g[q] = itizihozon;
+                        }
+                        else//(q=0)1回目,(q=1)2回目,(q=2)3回目,
+                        {
+                            g[q] = g[q+1]; 
+                        }
+                    } 
+                }
+            }
+            //配列のコピー
+            Array.Copy(qqq, g, qqq.Length);
+            //合計の表示
+            return goukei;       
+        }
+    }
+}
